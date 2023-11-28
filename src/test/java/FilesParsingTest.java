e9107862c3f6bce077282e52f502159c19191e57
Valeria Karandasheva <karandasheva.valeria@yandex.ru>
2023-11-28 23:11:01 +0300

    tests added

--- src/test/java/FilesParsingTest.java
+++ src/test/java/FilesParsingTest.java
@@ -1,2 +1,63 @@
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FilesParsingTest {
-}
+    private ClassLoader cl =FilesParsingTest.class.getClassLoader();
+    @Test
+    void pfdParsingTest() throws Exception {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+        File downloaded = $("[href*='junit-user-guide-5.10.1.pdf']").download();
+        PDF pdf = new PDF(downloaded);
+        Assertions.assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein", pdf.author);
+    }
+
+    @Test
+    void xlsParsingTest() throws Exception {
+        open("https://excelvba.ru/programmes/Teachers?ysclid=lfcu77j9j9951587711");
+        File downloaded = $("[href='https://ExcelVBA.ru/sites/default/files/teachers.xls']").download();
+        XLS xls = new XLS(downloaded);
+        String actualValue = xls.excel.getSheetAt(0).getRow(3).getCell(2).getStringCellValue();
+        Assertions.assertTrue(actualValue.contains("Суммарное количество часов планируемое на штатную по всем разделам"));
+    }
+    @Test
+    void csvFileParsingTest() throws Exception
+    {
+        try (InputStream is = cl.getResourceAsStream("example.csv");
+             CSVReader csvReader = new CSVReader(new InputStreamReader(is))){
+            List<String[]> data = csvReader.readAll();
+            Assertions.assertEquals(2, data.size());
+            Assertions.assertArrayEquals(
+                    new String[] {"Selenide", "https://selenide.org"},
+                    data.get(0)
+            );
+            Assertions.assertArrayEquals(
+                    new String[] {"JUnit 5", "https://junit.org"},
+                    data.get(1)
+            );
+
+}} @Test
+    void zipFileParsingTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("sample.zip")
+        )) {
+            ZipEntry entry;
+
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());
+            }
+        }
+    }}
